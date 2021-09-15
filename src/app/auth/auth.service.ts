eb0c21e2b8f6dd8aef28e5e7771c455a9102a955
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-15 21:13:23 +0600

    baseurl added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RegisterRequest } from './signup/register-request';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private baseUrl = environment.apiBaseUrl;
+
   constructor(private httpClient: HttpClient) {}
 
   signUp(registerRequest: RegisterRequest): Observable<any> {
     return this.httpClient.post(
-      'http://localhost:8080/api/auth/register',
+      `${this.baseUrl}/auth/register`,
       registerRequest,
       { responseType: 'text' }
     );
