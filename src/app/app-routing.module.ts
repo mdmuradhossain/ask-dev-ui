9b93bb8408969433d9f45452aa2a8271068d5fb3
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-09-20 20:20:09 +0600

    posts/add and categories/add routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { CreateCategoryComponent } from './category/create-category/create-category.component';
 import { HomeComponent } from './home/home.component';
+import { CreatePostComponent } from './post/create-post/create-post.component';
 import { PostComponent } from './post/post.component';
 
 const routes: Routes = [
@@ -15,6 +17,14 @@ const routes: Routes = [
   {
     path: 'posts',
     component: PostComponent,
+  },
+  {
+    path: 'posts/add',
+    component: CreatePostComponent,
+  },
+  {
+    path: 'categories/add',
+    component: CreateCategoryComponent,
   },
 ];
 
